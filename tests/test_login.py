427a609fe53e4a0e43a5f4f5233870ae58a65081
Raanan Adut <radesigner84@gmail.com>
2024-09-23 23:26:14 +0300

    create invalid login test

--- tests/test_login.py
+++ tests/test_login.py
@@ -24,4 +24,18 @@ def test_login():
     
     driver.quit()
     
+def test_invalid_login():
+    driver = webdriver.Chrome()
     
+    driver.get("https://www.saucedemo.com/")
+    
+    # Enter invalid credentials
+    driver.find_element(By.ID, "user-name").send_keys("wrong_user")
+    driver.find_element(By.ID, "password").send_keys("wrong_password")
+    
+    driver.find_element(By.ID, "login-button").click()
+    
+    error_message = driver.find_element(By.CSS_SELECTOR, ".error-message-container").text
+    assert "Username and password do not match" in error_message
+    
+    driver.quit()
