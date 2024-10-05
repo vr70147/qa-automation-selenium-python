d0f22c27d73365e9598135764790d78a861d4236
Raanan Adut <radesigner84@gmail.com>
2024-10-06 01:09:37 +0300

    Add cross-browser testing and Allure reporting to CI workflow

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,13 +3,17 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 import pytest
 
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome", help="Browser to run tests on")
+
 @pytest.fixture(params=["chrome", "firefox"])
 def browser(request):
-    if request.param == "chrome":
+    browser_choice = request.config.getoption("--browser")
+    if browser_choice == "chrome":
         options = ChromeOptions()
         options.add_argument("--headless")
         driver = webdriver.Chrome(options=options)
-    elif request.param == "firefox":
+    elif browser_choice == "firefox":
         options = FirefoxOptions()
         options.binary_location = '/usr/bin/firefox'
         options.add_argument("--headless")
